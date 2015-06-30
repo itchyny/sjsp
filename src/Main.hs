91570a44e7d613ac63a7e90cb0f5c11d37a04b1b
itchyny <itchyny@hatena.ne.jp>
2015-06-30 23:01:25 +0900

    print usage

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Applicative ((<$>), (<*>))
+import Data.Version (showVersion)
 import Language.JavaScript.Parser
 import System.Environment (getArgs)
 import System.FilePath.Posix (replaceExtension, takeFileName)
@@ -8,6 +9,14 @@ import qualified Data.ByteString.Builder as BS
 import qualified Data.ByteString.Lazy as BS
 
 import Injector (inject)
+import Paths_sjsp (version)
+
+main :: IO ()
+main = getArgs >>= mainProgram
+
+mainProgram :: [String] -> IO ()
+mainProgram [] = printUsage
+mainProgram args = mapM_ process args
 
 process :: String -> IO ()
 process name = BS.writeFile (replaceExtension name ".sjsp.js")
@@ -15,8 +24,20 @@ process name = BS.writeFile (replaceExtension name ".sjsp.js")
              . renderJS
            =<< inject (takeFileName name) <$> (lines <$> readFile name) <*> parseFile name
 
-mainProgram :: [String] -> IO ()
-mainProgram = mapM_ process
+printUsage :: IO ()
+printUsage = mapM_ putStrLn usage
 
-main :: IO ()
-main = getArgs >>= mainProgram
+usage :: [String]
+usage = [ "sjsp " ++ showVersion version ++ " Simple JavaScript Profiler"
+        , ""
+        , "Usage: sjsp [JavaScript files]"
+        , ""
+        , "Example:"
+        , "  sjsp test.js     # generates test.sjsp.js"
+        , ""
+        , ""
+        , "This software is released under the MIT License."
+        , "This software is distributed at https://github.com/itchyny/sjsp."
+        , "Report a bug of this software at https://github.com/itchyny/sjsp/issues."
+        , "The author is itchyny <https://github.com/itchyny>."
+        ]
